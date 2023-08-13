a0c1840e85fa414e6f8eac9abab01a058f61470a
Dani3lx <1danielxu@gmail.com>
2023-08-12 20:54:50 -0400

    added the analytic component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({
   weight: '400',
@@ -21,14 +22,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={`${inter.className} overflow-x-hidden`}>
-          <Navbar />
-          <div>
-            {children}
-            <Footer />
-          </div>
+        <Navbar />
+        <div>
+          {children}
+          <Footer />
+        </div>
       </body>
-
     </html >
   )
 }
