fe314113c9248542b71a9f5efa8988eb7ec87b41
Dani3lx <1danielxu@gmail.com>
2023-08-19 05:35:22 -0400

    Fixed styles

--- src/app/components/homepage/AboutSection.tsx
+++ src/app/components/homepage/AboutSection.tsx
@@ -28,12 +28,12 @@ function AboutSection() {
                     transition={{ delay: 0.15 }}>
                     <h1 className='dark:blue-gradient font-bold text-[40px] lg:text-[60px] z-10 text-shadow-blue drop-shadow-blue'>About Me.</h1>
                     <div className='flex w-full max-lg:flex-col'>
-                        <div className='lg:w-1/2 max-w-[800px] max-h-[600px] lg:-mt-16 max-lg:hidden'>
+                        <div className='z-10 lg:w-1/2 max-w-[800px] max-h-[600px] lg:-mt-16 max-lg:hidden max-xl:scale-75 max-xl:-ml-24'>
                             <Spline scene="https://prod.spline.design/Gbs38D6rSTrtN871/scene.splinecode" />
                         </div>
-                        <div className='w-full lg:w-1/2 gap-12 flex flex-col'>
+                        <div className='w-full lg:w-2/3 xl:w-1/2 gap-12 flex flex-col z-30'>
                             <div className='flex flex-col md:flex-row justify-center items-center w-full h-fit gap-10'>
-                                <div className='flex flex-col justify-center items-center h-fit gap-10 w-4/5 md:gap-10 sm:px-10 sm:py-5 xl:px-20 xl:py-10   dark:sm:bg-[#080808] sm:bg-[#eaeaea]  border-[#a0a0a026] sm:border-[0.1px] rounded-3xl xl:ml-12'>
+                                <div className='flex flex-col justify-center items-center h-fit gap-10 w-4/5 md:gap-10 sm:px-10 sm:py-5 xl:px-20 xl:py-10   dark:sm:bg-[#080808c0] sm:bg-[#eaeaeabf]  border-[#a0a0a026] sm:border-[0.1px] rounded-3xl xl:ml-12'>
                                     <div>
                                         <h1 className='dark:text-white font-semibold text-[20px] md:text-[30px] text-left md:text-left'>I&apos;m a student,</h1>
                                         <h1 className='dark:text-gray text-[15px] text-left'>Currently a third-year undergraduate student enrolled at the University of Toronto, working towards completing the Computer Science Specialist program alongside a minor in Mathematics.</h1>
